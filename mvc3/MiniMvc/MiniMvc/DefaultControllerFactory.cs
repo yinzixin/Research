68b902e6b6dea4c9f76a0008a6ce196e14d77d2b
Zixin Yin <yinzixin1985@hotmail.com>
2012-08-05 13:48:52 +0800

    add mini mvc solution http://www.cnblogs.com/artech/archive/2012/03/11/mvc-how-to-work-01.html

---
+++ mvc3/MiniMvc/MiniMvc/DefaultControllerFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Artech.MiniMvc
+{
+public class DefaultControllerFactory : IControllerFactory
+{
+    public IController CreateController(RequestContext requestContext, string controllerName)
+    {
+        string controllerType = controllerName + "Controller";
+        foreach (var ns in requestContext.RouteData.Namespaces)
+        {
+            string controllerTypeName = string.Format("{0}.{1}", ns, controllerType);
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                var controllerTypeQName = controllerTypeName + "," + assembly.FullName;
+                var type = Type.GetType(controllerTypeQName);
+                if (null != type)
+                {
+                    return (IController)Activator.CreateInstance(type);
+                }
+            }    
+        }
+        foreach(var ns in ControllerBuilder.Current.DefaultNamespaces)
+        {
+            string controllerTypeName = string.Format("{0}.{1}", ns, controllerType);
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                var controllerTypeQName = controllerTypeName + "," + assembly.FullName;
+                var type = Type.GetType(controllerTypeQName);
+                if (null != type)
+                {
+                    return (IController)Activator.CreateInstance(type);
+                }
+            }                
+        }
+        return null;
+    }
+}
+}
